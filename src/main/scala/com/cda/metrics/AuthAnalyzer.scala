5cebf2915d12b060825de9003ca1837bf8e70bff
Bill Barrington <bill_barrington@bellsouth.net>
2018-05-08 15:56:23 -0400

    Yay. It works. Need to now call PutMetricData.

--- src/main/scala/com/cda/metrics/AuthAnalyzer.scala
+++ src/main/scala/com/cda/metrics/AuthAnalyzer.scala
@@ -1,10 +1,14 @@
 package com.cda.metrics
 
-import java.time.{Duration, LocalDateTime}
+import java.time.{LocalDateTime, Duration => jDuration}
 import java.util.concurrent.LinkedBlockingQueue
 
 import com.cda._
 import org.slf4j.LoggerFactory
+import scalaz.-\/
+import scalaz.concurrent.Task
+
+import scala.concurrent.duration._
 
 /*
   // TODO
@@ -16,8 +20,10 @@ object AuthAnalyzer {
 
   val logger = LoggerFactory.getLogger(getClass)
 
-  // todo Use configuration file for value
-  val threshold = Duration.ofSeconds(30)
+  // todo Use configuration file for values
+  val threshold = jDuration.ofSeconds(30)
+  val metricsInterval = Duration(1, MINUTES)
+
 
   // All auth failures are sent to this AuthAnalyzer.
   // The 'token' is unique for a request and is used to match any
@@ -32,7 +38,7 @@ object AuthAnalyzer {
   private var map = Map.empty[String, FailureCount]
 
   private def withinThreshold(start: LocalDateTime, end: LocalDateTime): Boolean =
-    Duration.between(start, end).compareTo(threshold) <= 0
+    jDuration.between(start, end).compareTo(threshold) <= 0
 
 
   private var active = true
@@ -48,18 +54,37 @@ object AuthAnalyzer {
     If a entry with a matching token is found and the timestamps are NOT within the threshold,
     leave the count as is, but update the timestamp.
    */
-  private def update(failure: FailureEvent): Unit = map.get(failure.token) match {
-    case Some(count) =>
-      if (withinThreshold(count.timestamp, failure.timestamp))
-        map += (failure.token -> FailureCount(failure.timestamp, count.count + 1))
-      else map += (failure.token -> FailureCount(failure.timestamp, count.count))
+  private def update(failure: FailureEvent): Unit = map.synchronized {
+    map.get(failure.token) match {
+      case Some(count) =>
+        if (withinThreshold(count.timestamp, failure.timestamp))
+          map += (failure.token -> FailureCount(failure.timestamp, count.count + 1))
+        else map += (failure.token -> FailureCount(failure.timestamp, count.count))
 
-    case None => map += (failure.token -> FailureCount(failure.timestamp))
+      case None => map += (failure.token -> FailureCount(failure.timestamp))
+    }
   }
 
+  // Obtain a copy of the map containing all entries with a count > 0 and reset the map
+  private def resetMap(): Map[String, FailureCount] = map.synchronized {
+    val filtered = map.filter { case (_, failure) => failure.count > 0 }
+    map = map.empty
+    filtered
+  }
 
-  // TODO
-  // Create and schedule task to run every 5 minutes to create failed authentication metrics
+  // Create and schedule task to run periodically to create failed authentication metrics
+  private def scheduleMetrics(): Unit =
+    Task.schedule(doMetrics(), metricsInterval).unsafePerformAsync(_ match {
+      case -\/(thr) => logger.error(s"Metrics calc failed - ${asString(thr)}")
+      case _ =>
+    })
+
+  private def doMetrics(): Unit = {
+    val failures = resetMap()
+    val count = failures.foldLeft(0) { case (z, (_, fc)) => z + fc.count }
+    logger.info(s"$count auth failures in $metricsInterval")
+    scheduleMetrics()
+  }
 
 
   private val runnable = new Runnable {
@@ -78,4 +103,5 @@ object AuthAnalyzer {
 
   new Thread(runnable).start()
 
+  scheduleMetrics()
 }

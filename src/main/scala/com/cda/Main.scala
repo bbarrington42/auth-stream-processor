52ea185729763bdf1f4150178de1e2424d6592c9
Bill Barrington <bill_barrington@bellsouth.net>
2018-05-31 07:33:00 -0400

    More logging. Currently, app starts but does nothing.

--- src/main/scala/com/cda/Main.scala
+++ src/main/scala/com/cda/Main.scala
@@ -16,19 +16,6 @@ import scala.collection.JavaConverters._
 
 object Main {
 
-  def createWorker(env: String): Worker = {
-    val workerId = InetAddress.getLocalHost.getCanonicalHostName + ":" + UUID.randomUUID
-    val recordProcessorFactory = new IRecordProcessorFactory {
-      override def createProcessor(): IRecordProcessor = new RecordProcessor(AuthAnalyzer(env))
-    }
-    val config =
-      new KinesisClientLibConfiguration(
-        "auth-stream-processor-" + env,
-        env + "LogStream",
-        credentialsProvider,
-        workerId)
-    new Worker.Builder().recordProcessorFactory(recordProcessorFactory).config(config).build()
-  }
 
   val logger = LoggerFactory.getLogger(getClass)
 
@@ -36,6 +23,27 @@ object Main {
 
   // The role associated with the EC2 instance will provide our credentials
   val credentialsProvider = InstanceProfileCredentialsProvider.getInstance()
+
+
+  def createWorker(env: String): Worker = {
+    val workerId = InetAddress.getLocalHost.getCanonicalHostName + ":" + UUID.randomUUID
+
+    val recordProcessorFactory = new IRecordProcessorFactory {
+      override def createProcessor(): IRecordProcessor = {
+        logger.debug(s"Creating RecordProcessor for env $env")
+        new RecordProcessor(AuthAnalyzer(env))
+      }
+    }
+
+    val config =
+      new KinesisClientLibConfiguration(
+        "auth-stream-processor-" + env,
+        env + "LogStream",
+        credentialsProvider,
+        workerId)
+    logger.info(s"Creating Worker for env $env with workerId $workerId")
+    new Worker.Builder().recordProcessorFactory(recordProcessorFactory).config(config).build()
+  }
 
 
   def main(args: Array[String]): Unit = try {
